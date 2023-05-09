117280c875afd582647b12f1258fd51567e482c5
Gomes <guiguigcarneiro@gmail.com>
2023-05-08 22:28:27 -0300

    adicionando paginas no site

--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    meu_nome = "Gomes"
+    lista_frutas = ["Laranja", "Maça", "Banana"]
+    context = {
+        "nome" : meu_nome,
+    }
+    return render(request, "index.html", context)
